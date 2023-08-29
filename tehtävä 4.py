033638dd39c9907859ce83bc860a0f59140f48fb
karripartanen <karripar@metropolia.fi>
2023-08-29 13:26:31 +0300

    muutin tehtävää 4

--- tehtävä 4.py
+++ tehtävä 4.py
@@ -1,14 +1,11 @@
 Year = int(input("Antaisitko haluamasi vuosiluvun, kerron onko se karkausvuosi: "))
 
-if Year % 4 == 0:
+if Year % 4 == 0 or Year % 100 == 0 and Year % 400 == 0:
     print("Vuosi "+str(Year)+" on karkausvuosi")
-elif Year % 4 != 0:
+else:
     print("Vuosi "+str(Year)+" ei ole karkausvuosi")
 
-elif Year % 100 == 0 and Year % 400 == 0:
-    print("Vuosi " + str(Year) + " on karkausvuosi")
-elif Year % 100 != 0 and Year % 400 != 0:
-    print("Vuosi " + str(Year) + " on karkausvuosi")
+
 
 
 
